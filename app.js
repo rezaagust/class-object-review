7a9d65ce36dedd2de36e348f88758451b34035c3
rezaagust <reza.2020@smk-strada.sch.id>
2021-09-08 12:52:43 +0700

    tugas reza

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // Tulis kode di bawah ini
+// Membuat object literal
+let hewan = {
+    nama: "Anjing",
+    warna: "putih",
+    mata: 2,
+    kaki: 4,
+    tipe: "pitbull",
+    bagus: true,
+};
+
+console.log(hewan);
+console.log(hewan.warna);
+console.log(hewan.nama);
+
+// Constructor
+class Anjing {
+    constructor(tipeAnjing, modelAnjing) {
+        this.tipeAnjing = tipeAnjing;
+        this.modelAnjing = modelAnjing;
+    }
+}
+
+class Hewan {
+    constructor(nama, warna, beratGram, harga) {
+        this.nama = nama;
+        this.warna = warna;
+        this.beratGram = beratGram;
+        this.harga = harga;
+        this.kondisiBagus = false;
+    }
+
+    cekKondisiBagus(kondisi = false) {
+        return this.kondisiBagus = kondisi;
+    }
+}
+
+// Cara membuat object instance
+
+let hewanAnjing = new Hewan("Anjing", "Putih", 37, 100000);
+hewanAnjing.cekKondisiBagus(true);
+console.log(hewanAnjing);
